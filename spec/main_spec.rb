dc91185778be4dfeb73de657b90751311cddbe86
Rubén López <ruben@cs1.com>
2012-02-14 18:44:57 -0500

    .

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,41 +3,98 @@
 
 
 
-# 		describe "#emulate_keystroke" do
-# 			it "no parameter passed" do
-# 				pending
-# 			end
+require "spec_helper.rb"
 
-# 			it "parameter passed" do
-# 				pending
-# 			end
-# 		end
+describe InstallApplication, "Running an application and getting it's output and exit code" do
+	
+	describe "#check_requirements?"
+		it "returns true or false depending on symbol passed"
+		it "complains when its required argument is not passed"
+		it "checks if xcode is installed"
+		it "checks if the application folder is present"
+		it "checks if the customizations folder is present"
+		it "checks that the current OS is OSX Lion"
+	end
 
-# 		describe "#install_application" do
+	describe "#install_application"
+		it "installs brew" do
+			pending
+		end
+		it "installs rvm" do
+			pending
+		end
+		it "installs ruby" do
+			pending
+		end
+		it "returns exitcode 0 (SUCCESS)" do
+			pending
+		end
 
-# 			it "no parameter passed" do
-# 				pending
-# 			end
+	end
 
-# 			it "parameter was passed" do
-# 				pending
-# 			end
-			
-# 			it "application did not run properly" do
-# 				pending
-# 			end	
+	describe "#extract_files"
+		it "extracts .tar files" do
+			pending
+		end
+		it "extracts .zip files" do
+			pending
+		end
+		it "extracts .tar.gz files" do
+			pending
+		end
+		it "returns exitcode 0 (SUCCESS)" do
+			pending
+		end
+	end
 
-# 			it "application ran succesfully" do
-# 				pending
-# 			end
+	describe "#install_brew_apps"
+		it "complains when its required argument is not passed" do
+			pending
+		end
+		it "accepts an array" do
+			pending
+		end
+		it "does post installation for mysql" do
+			pending
+		end
+		it "returns exitcode 0 (SUCCESS)" do
+			pending
+		end
+	end
 
+	describe "#application_installed?"
+		it "returns true" do
+			pending
+		end
+		it "returns false" do
+			pending
+		end
+	end
 
-# 		end
+	describe "#init_check_requirements" do
+		it "checks for the presence of Applications folder"
+		it "check for the presence of Customization folder"
+	end
 
-# 		describe "#install_packages" do
-# 			pending
-# 		end
-# end
+	describe "#init_additional_applications" do
+		it "complains when its required argument is not passed"
+		it "returns exitcode 0 (SUCCESS)"
+	end
 
+	describe "#install_by_copy" do
+		it "complains when its required argument is not passed"
+		it "installs .app files"
+		it "install screen savers"
+		it "installs fonts"
+		it "installs packages"
+		it "returns exitcode 0 (SUCCESS)"
+	end
+
+	describe "#install_draggable_applications"
+	it "complains when its requirement argument is not passed"
+	it "returns exitcode 0"
+	end
+
+end # END OF parent describe block
 
 
