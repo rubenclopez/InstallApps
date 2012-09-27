f754a1612751c60b35772c01d41a8565b063d1c0
Rubén López <ruben@cs1.com>
2012-09-27 12:17:27 -0600

    Added app.rb file.

--- app.rb
+++ app.rb
@@ -1,35 +1,6 @@
-#!/usr/bin/env ruby
+require "exec_application.rb"
 
-## TODO: Add a way to install .pkg files by themselves. 
-
-
-
-require 'fileutils'
-require 'rubygems'
-require 'popen4'
-
-class String 
-  def f_extension
-    File.extname self
-  end
-
-  def f_name
-    File.basename self
-  end
-
-  def f_base
-    f_name.split(".").first
-  end
-
-  def f_abs_path
-    ## This seems over-kill, and I missing something???
-    path = File.expand_path(self).split("/")
-    path.pop
-    path.join("/")
-  end
-end
-
-module InstallApps
+module Main
 
   def self.check_requirements?(app)
     
@@ -52,12 +23,13 @@ module InstallApps
   def self.install_application(app)
     case app
       when :xcode
-        install_output = ExecApplication::init('sudo installer -pkg ./Applications/Install\\ Xcode.app/Contents/Resources/Xcode.mpkg -target /') 
+             
+        install_output = ExecApplication::init('sudo installer -verbose -pkg ./Applications/Install\\ Xcode.app/Contents/Resources/Xcode.mpkg -target /') 
       when :brew
         current_user = `whoami`.chomp
         ExecApplication::init('sudo mkdir -p /usr/local/Cellar') 
         ExecApplication::init('sudo chown -R "#{current_user}":staff /usr/local/Cellar')
-
+              
         install_output = ExecApplication::init("/usr/bin/ruby -e $(curl -fsSL https://raw.github.com/gist/323731) & osascript ../../AppleScript/simulate_key.script") 
         ExecAppli
         puts "Installing Brew Apps: ack, wget, curl, redis, memcached, libmemcached, colordiff, imagemagick... "
@@ -75,12 +47,19 @@ module InstallApps
     end
   end
 
-  def self.extract_files(zip, extract_location)
-    case zip
-      when /.tar$/
-        print "Extracting #{zip} into #{extract_location}... "
-        extract_output = ExecApplication::init("mkdir -p ~/.ssh && tar -xf ./Personal/#{zip} -C ~/.ssh") 
-      puts "[DONE]"
+  def self.extract_files(file, extract_location)
+    #puts "Extracting #{file} into #{extract_location}... "
+
+    case file
+      when /\.tar$/
+        extract_output = ExecApplication::init("mkdir -p #{extract_location} && tar -xf #{file} -C #{extract_location}") 
+      when /\.zip$/
+        extract_output = ExecApplication::init("mkdir -p #{extract_location} && unzip #{file} -d #{extract_location}") 
+      when /\.tar\.gz$/
+        extract_output = ExecApplication::init("mkdir -p #{extract_location} && tar -zxf #{file} -C #{extract_location}") 
+      else
+        false
+      #puts "[DONE]"
     end
   end
 
@@ -105,8 +84,13 @@ module InstallApps
 
   
   def self.application_installed?(app)
-    File.exists?(app)
+    path  = app.include?('/Applications/') ? '' : '/Applications/' || '/Applications/'
+    ext   = app.include?('.app') ? '' : '.app' || '.app'
+
+      File.exists? "#{path}#{app}#{ext}"
   end  
+
+
 
 
   def self.init_check_requirements
@@ -212,63 +196,3 @@ module InstallApps
 
 
 end ### END FOR MODULE
-
-def emulate_keystroke(key)
-  #["tell application "System Events"]
-end
-
-
-  # puts "Please press enter twice to install" # Figure out a way to not need to do this
-  #     puts `/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"`
-  #     puts "Installing Brew Apps: ack, wget, curl, redis, memcached, libmemcached, colordiff, imagemagick... "
-  #     `brew install ack wget curl redis memcached libmemcached colordiff imagemagick`
-  #   when :rvm
-
-
-def install_packages(app)
-         `installer -pkg "#{app}" -target /`
-end
-
-
-puts 
-
-puts "This application installed the primary applications I use in my normal every day computer"
-puts 
-print "Sudoing... "
-`sudo ls`
-
-print "Checking for OSX Lion... "
-puts check_requirements?(:OSVersion) ? "[FOUND]" : "[ERROR.. This system is not running OSX Lion]"
-
-print "Installing personal system files..."
-extract_files("ssl_key.tar", "~/.ssh/")
-
-
-print "Checking for Xcode... "
-if check_requirements?(:xcode) 
-  puts "[FOUND]"
-else
-  puts "[ERROR.. Not found]"
-  puts "Installing Xcode 4.2.2... Notice: This will take a while"
-  install_application(:xcode)
-  puts "[DONE]"
-end
-
-puts "Installing brew... "
-#install_application(:brew)
-
-puts "Install brew applications... "
-apps = %w|mysql|
-#install_brew_apps(apps)
-print "[DONE]"
-
-puts "Installing RVM... "
-#install_application(:rvm)
-print "[DONE]"
-
-
-print "Installing ruby 1.9.3"
-install_application(:ruby)
-
-
-
